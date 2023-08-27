71cf41e7f7beea3bcac2df4726a6638872520e74
perumal83733 <137146583+perumal83733@users.noreply.github.com>
2023-08-27 17:27:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -18,7 +18,7 @@ variable "PUBLIC_SUBNET1_CIDR_BLOCK" {
 variable "PUBLIC_SUBNET2_CIDR_BLOCK" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.2.0/24"
+  default     = "10.0.102.0/24"
 }
 
 variable "ENVIRONMENT" {
