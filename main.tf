c15d506b1a60ec73984f908a9cd0de4524a14070
perumal83733 <137146583+perumal83733@users.noreply.github.com>
2023-08-27 17:07:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,10 +107,11 @@ resource "aws_lb" "AppLB" {
   name               = "AppLoadbalancer"
   internal           = false
   load_balancer_type = "application"
+  security_groups     = [aws_security_group.instance_sg.id]
   
     subnets = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id,
+    "aws_subnet.public_subnet_1.id",
+    "aws_subnet.public_subnet_2.id",
   ]
   
 
